795a28e396e97f5c9be99ee17aaeb3eefe28beaf
Cadamir <ischdu0@gmail.com>
2022-07-14 00:02:02 +0200

    Bilder

--- training-app/src/app/app.module.ts
+++ training-app/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TrainingComponent } from './training/training.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    CalendarComponent,
+    TrainingComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
