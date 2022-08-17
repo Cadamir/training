b3cd32c2e5ca9e8a292775a3214ec0a50d0e6f29
Cadamir <ischdu0@gmail.com>
2022-08-17 14:26:31 +0200

    bug fix

--- training-app/src/app/services/training/training.service.ts
+++ training-app/src/app/services/training/training.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TrainingClass } from 'src/app/models/training/training-class';
 import { Observable } from 'rxjs';
 
@@ -13,7 +13,11 @@ export class TrainingService {
   public getTrainings(num:number):Observable<TrainingClass[]>{
     console.log("start request");
 
-    let req = this.http.get<TrainingClass[]>("http://localhost:8080/training?number=20");
+    let req = this.http.get<TrainingClass[]>("http://localhost:8080/training?number=20", {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:8080'
+    })});
     return req;
   }
 }
--- trainings-ws/src/main/java/cad/trainingsws/AthletController.java
+++ trainings-ws/src/main/java/cad/trainingsws/AthletController.java
@@ -1,10 +1,12 @@
 package cad.trainingsws;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AthletController {
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/athlet")
     public Athlet athlet(){
         return Athlet.randomFull();
